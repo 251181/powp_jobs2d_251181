de322bbe4c75348ce78819faa29799724a1b82d7
251181 <251181@edu.p.lodz.pl>
2026-03-19 11:13:42 +0100

    Lab2 Wykonanie zadania 3

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.command;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+    private final List<DriverCommand> commands = new ArrayList<>();
+
+    void addCommand(DriverCommand command) {
+        commands.add(command);
+    }
+
+    @Override
+    public void execute() {
+        for (DriverCommand command: commands){
+            command.execute();
+        }
+    }
+}
